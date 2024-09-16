5653a283f01d82cb3d2c0430affda5a445d5d001
paullosergio <paulomg1996@gmail.com>
2024-09-16 20:47:11 -0300

    :sparkles: Feat: Add Server Initialization

---
+++ server/src/http/server.ts
@@ -0,0 +1,29 @@
+import fastify from 'fastify'
+import {
+	serializerCompiler,
+	validatorCompiler,
+	type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { createGoalRoute } from '../routes/create-goal'
+import { createCompletionRoute } from '../routes/create-completion'
+import { getPendingGoalRoute } from '../routes/get-pending-goals'
+import { getWeekSummaryRoute } from '../routes/get-week-summary'
+import fastifyCors from '@fastify/cors'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifyCors, {
+	origin: '*',
+})
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(createGoalRoute)
+app.register(createCompletionRoute)
+app.register(getPendingGoalRoute)
+app.register(getWeekSummaryRoute)
+
+app.listen({ host: '0.0.0.0', port: 3333 }).then(() => {
+	console.log('Server is running!')
+})

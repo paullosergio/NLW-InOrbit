3a79dafed9e07a65c892bbbc9c0078604df9ba8b
paullosergio <paulomg1996@gmail.com>
2024-09-19 12:00:17 -0300

    :hammer: Refactor: Simplify function

--- server/src/routes/get-pending-goals.ts
+++ server/src/routes/get-pending-goals.ts
@@ -1,12 +1,8 @@
-import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod';
-import { getWeekPendingGoals } from '../functions/get-week-pending-goals';
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import { getWeekPendingGoals } from '../functions/get-week-pending-goals'
 
-export const getPendingGoalRoute: FastifyPluginAsyncZod = async (app) =>{
-  
-  app.get('/pending-goals', async () => {
-    const { pendingGoals } = await getWeekPendingGoals()
-  
-    return { pendingGoals }
-  })
-
-};
+export const getPendingGoalRoute: FastifyPluginAsyncZod = async app => {
+	app.get('/pending-goals', async () => {
+		return await getWeekPendingGoals()
+	})
+}
--- server/src/routes/get-week-summary.ts
+++ server/src/routes/get-week-summary.ts
@@ -2,9 +2,7 @@ import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import { getWeekSummary } from '../functions/get-week-summary'
 
 export const getWeekSummaryRoute: FastifyPluginAsyncZod = async app => {
-  app.get('/summary', async () => {
-    const { summary } = await getWeekSummary()
-
-    return { summary }
-  })
+	app.get('/summary', async () => {
+		return await getWeekSummary()
+	})
 }

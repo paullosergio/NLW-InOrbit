24a9f70db3d31e926005041cb6d723e54a2c454f
paullosergio <paulomg1996@gmail.com>
2024-09-19 11:55:38 -0300

    :hammer: Refactor: Add handle errors

--- server/src/functions/create-goal-completion.ts
+++ server/src/functions/create-goal-completion.ts
@@ -2,52 +2,59 @@ import { and, count, eq, gte, lte, sql } from 'drizzle-orm'
 import { db } from '../db'
 import { goalCompletions, goals } from '../db/schema'
 import dayjs from 'dayjs'
+import { HttpError } from '../http-error-class'
 
 interface CreateGoalCompletionRequeste {
-  goalId: string
+	goalId: string
 }
 
 export async function createGoalCompletion({
-  goalId,
+	goalId,
 }: CreateGoalCompletionRequeste) {
-  const firstDayOfWeek = dayjs().startOf('week').toDate()
-  const lastDayOfWeek = dayjs().endOf('week').toDate()
+	const goal = await db.select().from(goals).where(eq(goals.id, goalId))
 
-  const goalCompletionCounts = db.$with('goal_completion_counts').as(
-    db
-      .select({
-        goalId: goalCompletions.goalId,
-        completionCount: count(goalCompletions.id).as('completionCount'),
-      })
-      .from(goalCompletions)
-      .where(
-        and(
-          gte(goalCompletions.createdAt, firstDayOfWeek),
-          lte(goalCompletions.createdAt, lastDayOfWeek),
-          eq(goalCompletions.goalId, goalId)
-        )
-      )
-      .groupBy(goalCompletions.goalId)
-  )
-  const result = await db
-    .with(goalCompletionCounts)
-    .select({
-      desiredWeeklyFrequency: goals.desiredWeeklyFrequency,
-      completionCount:
-        sql`COALESCE(${goalCompletionCounts.completionCount}, 0)`.mapWith(
-          Number
-        ),
-    })
-    .from(goals)
-    .leftJoin(goalCompletionCounts, eq(goalCompletionCounts.goalId, goals.id))
-    .where(eq(goals.id, goalId))
-    .limit(1)
+	if (goal.length === 0) {
+		throw new HttpError(404, 'Goal not found') // Not found status code
+	}
 
-  const { completionCount, desiredWeeklyFrequency } = result[0]
+	const firstDayOfWeek = dayjs().startOf('week').toDate()
+	const lastDayOfWeek = dayjs().endOf('week').toDate()
 
-  if (completionCount >= desiredWeeklyFrequency) {
-    throw new Error('Goal already completed this week')
-  }
+	const goalCompletionCounts = db.$with('goal_completion_counts').as(
+		db
+			.select({
+				goalId: goalCompletions.goalId,
+				completionCount: count(goalCompletions.id).as('completionCount'),
+			})
+			.from(goalCompletions)
+			.where(
+				and(
+					gte(goalCompletions.createdAt, firstDayOfWeek),
+					lte(goalCompletions.createdAt, lastDayOfWeek),
+					eq(goalCompletions.goalId, goalId)
+				)
+			)
+			.groupBy(goalCompletions.goalId)
+	)
+	const result = await db
+		.with(goalCompletionCounts)
+		.select({
+			desiredWeeklyFrequency: goals.desiredWeeklyFrequency,
+			completionCount:
+				sql`COALESCE(${goalCompletionCounts.completionCount}, 0)`.mapWith(
+					Number
+				),
+		})
+		.from(goals)
+		.leftJoin(goalCompletionCounts, eq(goalCompletionCounts.goalId, goals.id))
+		.where(eq(goals.id, goalId))
+		.limit(1)
 
-  await db.insert(goalCompletions).values({goalId})
+	const { completionCount, desiredWeeklyFrequency } = result[0]
+
+	if (completionCount >= desiredWeeklyFrequency) {
+		throw new HttpError(400, 'Goal already completed this week')
+	}
+
+	await db.insert(goalCompletions).values({ goalId })
 }
--- server/src/routes/create-completion.ts
+++ server/src/routes/create-completion.ts
@@ -1,23 +1,33 @@
-import { z } from 'zod';
-import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod';
-import { createGoalCompletion } from '../functions/create-goal-completion';
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+import { createGoalCompletion } from '../functions/create-goal-completion'
+import { HttpError } from '../http-error-class'
 
-export const createCompletionRoute: FastifyPluginAsyncZod = async (app) =>{
-  app.post(
-    '/completions',
-    {
-      schema: {
-        body: z.object({
-          goalId: z.string(),
-        }),
-      },
-    },
-  
-    async request => {
-      const { goalId } = request.body
-  
-      await createGoalCompletion({ goalId })
-    }
-  )
+export const createCompletionRoute: FastifyPluginAsyncZod = async app => {
+	app.post(
+		'/completions',
+		{
+			schema: {
+				body: z.object({
+					goalId: z.string(),
+				}),
+			},
+		},
 
-};
+		async (request, reply) => {
+			try {
+				const { goalId } = request.body
+
+				await createGoalCompletion({ goalId })
+
+				reply.status(200).send({ message: 'Completion created successfully' })
+			} catch (error) {
+				if (error instanceof HttpError) {
+					reply.status(error.statusCode).send({ message: error.message })
+				} else {
+					reply.status(500).send({ message: 'Internal Server Error' })
+				}
+			}
+		}
+	)
+}
